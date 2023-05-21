bdc26a0693bc0d3bfb75197411f062a657e0de4f
barburakka <barburakka@gmail.com>
2023-05-21 21:44:37 +0100

    Exercise 1.6

---
+++ part1/unicafe/src/App.js
@@ -0,0 +1,29 @@
+import { useState } from 'react'
+
+const Heading = ({text}) => <h2>{text}</h2>
+
+const App = () => {
+  const [good, setGood] = useState(0)
+  const [neutral, setNeutral] = useState(0)
+  const [bad, setBad] = useState(0)
+
+  const prompt = 'Please rate your experience below:'
+  const stats = 'Statistics'
+
+  return (
+    <div>
+      <Heading text={prompt} />
+      <button onClick={() => setGood(good + 1)}>Good</button>
+      &nbsp;
+      <button onClick={() => setNeutral(neutral + 1)}>Neutral</button>
+      &nbsp;
+      <button onClick={() => setBad(bad + 1)}>Bad</button>
+      <Heading text={stats} />
+      <p>Good: {good}</p>
+      <p>Neutral: {neutral}</p>
+      <p>Bad: {bad}</p>
+    </div>
+  )
+}
+
+export default App;
